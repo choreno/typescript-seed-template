f45680f880a2b060030a2bdeea7c220eff594c83
THBar <choreno@gmail.com>
2016-09-30 16:50:14 -0400

    training ...

--- dist/main.js
+++ dist/main.js
@@ -1,118 +1,96 @@
-interface IPerson {
-
-    getPersonCategory(): string;
-    canSignContracts(): boolean;
-    getDateOfBirth(): string;
-}
-
-
-class Person {
-
-    _dateOfBirth: Date;
-    constructor(dateOfBirth: Date) {
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Person = (function () {
+    function Person(dateOfBirth) {
         this._dateOfBirth = dateOfBirth;
     }
-
-    getDateOfBirth(): string {
+    Person.prototype.getDateOfBirth = function () {
         return this._dateOfBirth.toDateString();
+    };
+    return Person;
+}());
+var Infant = (function (_super) {
+    __extends(Infant, _super);
+    function Infant() {
+        _super.apply(this, arguments);
     }
-
-}
-
-
-class Infant extends Person implements IPerson {
-    getPersonCategory(): string {
+    Infant.prototype.getPersonCategory = function () {
         return "Infant";
-
-    }
-    canSignContracts(): boolean {
+    };
+    Infant.prototype.canSignContracts = function () {
         return false;
+    };
+    return Infant;
+}(Person));
+var Child = (function (_super) {
+    __extends(Child, _super);
+    function Child() {
+        _super.apply(this, arguments);
     }
-}
-
-class Child extends Person implements IPerson {
-    getPersonCategory(): string {
+    Child.prototype.getPersonCategory = function () {
         return "Child";
+    };
+    Child.prototype.canSignContracts = function () { return false; };
+    return Child;
+}(Person));
+var Adult = (function (_super) {
+    __extends(Adult, _super);
+    function Adult() {
+        _super.apply(this, arguments);
     }
-    canSignContracts() { return false; }
-}
-class Adult extends Person implements IPerson {
-    getPersonCategory(): string {
+    Adult.prototype.getPersonCategory = function () {
         return "Adult";
+    };
+    Adult.prototype.canSignContracts = function () { return true; };
+    return Adult;
+}(Person));
+var PersonFactory = (function () {
+    function PersonFactory() {
     }
-    canSignContracts() { return true; }
-}
-
-
-class PersonFactory {
-
-    getPerson(dateOfBirth: Date): IPerson {
-
+    PersonFactory.prototype.getPerson = function (dateOfBirth) {
         var dateNow = new Date();
-        var dateTwoYearsAgo = new Date(dateNow.getFullYear() - 2,
-            dateNow.getMonth(), dateNow.getDay());
-
-        var dateEighteenYearsAgo = new Date(dateNow.getFullYear() - 18,
-            dateNow.getMonth(), dateNow.getDay());
-
+        var dateTwoYearsAgo = new Date(dateNow.getFullYear() - 2, dateNow.getMonth(), dateNow.getDay());
+        var dateEighteenYearsAgo = new Date(dateNow.getFullYear() - 18, dateNow.getMonth(), dateNow.getDay());
         //Factory
         if (dateOfBirth >= dateTwoYearsAgo) {
             return new Infant(dateOfBirth);
         }
-
         if (dateOfBirth >= dateEighteenYearsAgo) {
             return new Child(dateOfBirth);
         }
-
         return new Adult(dateOfBirth);
-
-    }
-}
-
-
-
+    };
+    return PersonFactory;
+}());
 var personFactory = new PersonFactory();
-
-var personArray: IPerson[] = new Array();
+var personArray = new Array();
 personArray.push(personFactory.getPerson(new Date(2014, 09, 29))); // infant
 personArray.push(personFactory.getPerson(new Date(2000, 09, 29))); // child
 personArray.push(personFactory.getPerson(new Date(1950, 09, 29))); // adult
-
 for (var i = 0; i < personArray.length; i++) {
-    
     console.log(" A person with a birth date of :"
         + personArray[i].getDateOfBirth()
         + " is categorised as : "
         + personArray[i].getPersonCategory()
         + " and can sign : "
         + personArray[i].canSignContracts());
-
-
 }
-
 // interface IBase {
-
 //     id: number;
 // }
-
 // interface IDrivedFromBase extends IBase {
-
 //     name: string;
 // }
-
 // class BaseClass implements IBase {
 //     id: number;
 // }
-
-
 // class DerivedClass extends BaseClass implements IDrivedFromBase {
-
-
 //     name: string;
 // }
-
 // class BaseClassWithConstructor {
-
 //     private _id : number;
 //     constructor(id:number){
 //         this._id = id; 
@@ -120,26 +98,16 @@ for (var i = 0; i < personArray.length; i++) {
 //     getProperties(): string {
 //         return 'id:' + this._id; 
 //     } 
-
 // }
-
-
 // class DerivedClassWithConstructor extends BaseClassWithConstructor{
 //     private _name: string; 
 //     constructor(id: number, name:string){
 //         super(id);
 //         this._name  = name ;
-
 //     }
-
 //     getProperties():string{
 //         return "_name:" + this._name + '.' + super.getProperties(); 
 //     }
-
 // }
-
-
-
 // var myDerivedClass = new DerivedClassWithConstructor(1, "name");
-
-// console.log (myDerivedClass.getProperties());
+// console.log (myDerivedClass.getProperties()); 
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var shell = require('gulp-shell') ;
 
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
@@ -15,6 +14,8 @@ var path = {
 
 var browserSync = require('browser-sync').create();
 var del = require('del');
+var exec = require('child_process').exec;
+var runsequence = require('run-sequence');
 
 
 gulp.task('clean', function(){
@@ -48,13 +49,13 @@ gulp.task('browserSync',['browserify'], function () {
     browserSync.init({
         server: {
             baseDir: 'dist/'
-            //baseDir: '.'   //call index.html at current folder.
+            //baseDir: '.'   //call index.html at base directory
         }
     })
 })
 
 
-gulp.task('watches', ['browserSync'], function () {
+gulp.task('watch-web', ['browserSync'], function () {
     
     gulp.watch('*.html', ['browserify', browserSync.reload]);
     gulp.watch('src/**/*.ts', ['browserify', browserSync.reload ]);
@@ -63,8 +64,8 @@ gulp.task('watches', ['browserSync'], function () {
 
 
 
-//DEFAULT
-gulp.task('default', ['watches']);
+//For WebProject 
+gulp.task('web', ['watch-web']);
 
 
 
@@ -80,7 +81,38 @@ gulp.task('tscript', function () {
 
 
 //simple node execution
-gulp.task('exec', ['tscript'], shell.task("node dist/main.js")) ;
+//gulp.task('exec', ['tscript'], shell.task("node dist/main.js")) ;
+ 
+gulp.task('exec', function (cb) {
+  exec('node dist/main.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+
+gulp.task('watch-node', function () {
+
+    //Keep in mind watch can take an array of strings to watch for, don't call it multiple times.
+    gulp.watch(['src/**/*.ts', 'src/*.ts'], ['node']); //for make sure watch?????
+
+});
+
+//For Node Project
+gulp.task('node',function(){
+    runsequence('tscript','exec');
+})
+
+
+//-----------------------------------------------------------------
+//DEFAULT TASKS
+gulp.task('default',function(){
+    runsequence('watch-node','node');
+});
+//gulp.task('default',['web']);
+
+
 
 
 
