410430d876036bb9a55426bbf8cc9caf8b1bd5a7
THBar <choreno@gmail.com>
2016-11-07 11:05:45 -0500

    added them

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,12 @@
 ///<reference path="../typings/jquery/jquery.d.ts" />
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+console.log('Hello, ' + greeter.greet() + '!!!!!!!!!!!!!!!!');
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 ///<reference path="../typings/jquery/jquery.d.ts" />
+
+class Greeter {
+    private greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    public greet(): string {
+ 
+        
+        return "Hello, " + this.greeting;
+    }
+}
+var greeter: Greeter = new Greeter("world");
+ 
+console.log('Hello, ' + greeter.greet() + '!!!!!!!!!!!!!!!!');
